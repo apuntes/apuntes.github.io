103fb250626c5fe516d25ad501950b8841ee8212
nsm <smnicolas>
2017-07-25 22:58:06 -0300

    usando emacs org

---
+++ org/conf.el
@@ -0,0 +1,10 @@
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      `(("apuntes"
+         :base-directory "~/apuntes.github.io/org"
+         :recursive t
+         :publishing-directory "~/apuntes.github.io/"
+         :publishing-function org-html-publish-to-html)))
+
+(org-publish-current-project)        
